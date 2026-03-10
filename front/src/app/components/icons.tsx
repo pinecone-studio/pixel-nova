bf70dd3bf76665e817fa82303f21bc3ed8d518ba
bsunduibazzr <bsunduibazrr8@gmail.com>
2026-03-10 16:12:36 +0800

    contracts-part1

---
+++ front/src/app/components/icons.tsx
@@ -0,0 +1,149 @@
+export const DocumentIcon = () => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3.99999 14.6667C3.64637 14.6667 3.30723 14.5262 3.05718 14.2761C2.80713 14.0261 2.66666 13.687 2.66666 13.3333V2.66666C2.66666 2.31304 2.80713 1.9739 3.05718 1.72385C3.30723 1.47381 3.64637 1.33333 3.99999 1.33333H9.33332C9.54436 1.33299 9.75338 1.3744 9.94834 1.45518C10.1433 1.53595 10.3204 1.6545 10.4693 1.804L12.8613 4.196C13.0112 4.345 13.1301 4.52223 13.2111 4.71744C13.2921 4.91265 13.3337 5.12198 13.3333 5.33333V13.3333C13.3333 13.687 13.1928 14.0261 12.9428 14.2761C12.6928 14.5262 12.3536 14.6667 12 14.6667H3.99999Z"
+        stroke="#00C0A8"
+        strokeWidth="1.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.33334 1.33333V4.66666C9.33334 4.84347 9.40358 5.01304 9.52861 5.13807C9.65363 5.26309 9.8232 5.33333 10 5.33333H13.3333"
+        stroke="#00C0A8"
+        strokeWidth="1.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.66668 6H5.33334"
+        stroke="#00C0A8"
+        strokeWidth="1.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.6667 8.66667H5.33334"
+        stroke="#00C0A8"
+        strokeWidth="1.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.6667 11.3333H5.33334"
+        stroke="#00C0A8"
+        strokeWidth="1.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export const RequestIcon = () => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_42_399)">
+        <path
+          d="M9.69067 14.4573C9.716 14.5205 9.76003 14.5743 9.81685 14.6117C9.87368 14.6491 9.94057 14.6682 10.0086 14.6664C10.0766 14.6647 10.1424 14.6422 10.1972 14.6019C10.2521 14.5617 10.2933 14.5057 10.3153 14.4413L14.6487 1.77466C14.67 1.71559 14.6741 1.65167 14.6604 1.59037C14.6467 1.52907 14.6159 1.47293 14.5715 1.42852C14.5271 1.3841 14.4709 1.35326 14.4096 1.33959C14.3483 1.32592 14.2844 1.32999 14.2253 1.35133L1.55867 5.68466C1.49433 5.70673 1.43829 5.74794 1.39805 5.80278C1.35781 5.85761 1.33532 5.92344 1.33357 5.99144C1.33183 6.05943 1.35093 6.12632 1.38831 6.18315C1.42568 6.23997 1.47955 6.284 1.54267 6.30933L6.82934 8.42933C6.99646 8.49624 7.1483 8.5963 7.27571 8.72348C7.40312 8.85066 7.50346 9.00233 7.57067 9.16933L9.69067 14.4573Z"
+          stroke="#77818C"
+          strokeWidth="1.33333"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M14.5693 1.43134L7.276 8.724"
+          stroke="#77818C"
+          strokeWidth="1.33333"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_42_399">
+          <rect width="16" height="16" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export const FactIcon = () => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4 14.6667C3.64638 14.6667 3.30724 14.5262 3.05719 14.2761C2.80714 14.0261 2.66667 13.687 2.66667 13.3333V2.66666C2.66667 2.31304 2.80714 1.9739 3.05719 1.72385C3.30724 1.47381 3.64638 1.33333 4 1.33333H9.33333C9.54437 1.33299 9.75339 1.3744 9.94835 1.45518C10.1433 1.53595 10.3204 1.6545 10.4693 1.804L12.8613 4.196C13.0112 4.345 13.1301 4.52223 13.2111 4.71744C13.2921 4.91265 13.3337 5.12198 13.3333 5.33333V13.3333C13.3333 13.687 13.1929 14.0261 12.9428 14.2761C12.6928 14.5262 12.3536 14.6667 12 14.6667H4Z"
+        stroke="#77818C"
+        strokeWidth="1.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.33333 1.33333V4.66666C9.33333 4.84347 9.40357 5.01304 9.52859 5.13807C9.65362 5.26309 9.82319 5.33333 10 5.33333H13.3333"
+        stroke="#77818C"
+        strokeWidth="1.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M6.66667 6H5.33333"
+        stroke="#77818C"
+        strokeWidth="1.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.6667 8.66667H5.33333"
+        stroke="#77818C"
+        strokeWidth="1.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.6667 11.3333H5.33333"
+        stroke="#77818C"
+        strokeWidth="1.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export const DownIcon = () => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M4 6L8 10L12 6"
+        stroke="#77818C"
+        strokeWidth="1.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
