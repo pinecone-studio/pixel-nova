8b27f67b7b253cd6129abcc72c7d6a80f652098b
bsunduibazzr <bsunduibazrr8@gmail.com>
2026-03-17 11:34:08 +0800

    front: remove leave request types and queries

--- front/src/graphql/mutations/index.ts
+++ front/src/graphql/mutations/index.ts
@@ -1,7 +1,6 @@
 export * from "./auth";
 export * from "./documents";
 export * from "./employees";
-export * from "./leave-requests";
 export * from "./contract-requests";
 export * from "./profile";
 export * from "./notifications";
--- front/src/graphql/queries/index.ts
+++ front/src/graphql/queries/index.ts
@@ -2,6 +2,5 @@ export * from "./actions";
 export * from "./audit-logs";
 export * from "./documents";
 export * from "./employees";
-export * from "./leave-requests";
 export * from "./contract-requests";
 export * from "./notifications";
